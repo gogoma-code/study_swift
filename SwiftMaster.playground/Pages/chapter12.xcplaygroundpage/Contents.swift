e1f08eaa1fbde8c7e941a138b68bcfc84615525c
lyk-brave <officeyongki@gmail.com>
2020-08-07 12:01:47 +0900

    chapter12

---
+++ SwiftMaster.playground/Pages/chapter12.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+public struct SomeType {
+    private var privateVal = 1
+    fileprivate var fileprivateVal = 2
+}
+
+extension SomeType {
+    public func publicMethod() {
+        print("\(self.privateVal), \(self.fileprivateVal)")
+    }
+    
+    private func privateMethod() {
+        print("\(self.privateVal), \(self.fileprivateVal)")
+    }
+    
+    fileprivate func fileprivateMethod() {
+        print("\(self.privateVal), \(self.fileprivateVal)")
+    }
+}
+
+var someType: SomeType = SomeType()
+someType.publicMethod()
+//someType.privateMethod() //X
+someType.fileprivateMethod()
+print()
+
+struct AnotherType {
+    var someIns: SomeType = SomeType()
+    
+    mutating func someMethod() {
+        self.someIns.publicMethod()
+        
+        self.someIns.fileprivateVal = 200
+        self.someIns.fileprivateMethod()
+        
+        //self.someIns.privateVal = 100 // Access X
+        //self.someIns.privateMethod() // Access X
+    }
+}
+
+var another: AnotherType = AnotherType()
+another.someMethod()
