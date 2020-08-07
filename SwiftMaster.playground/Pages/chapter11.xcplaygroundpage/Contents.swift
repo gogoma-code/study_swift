6c18cae87a2c64a583d1254df454500db9f99952
lyk-brave <officeyongki@gmail.com>
2020-08-07 11:13:46 +0900

    chapter11

---
+++ SwiftMaster.playground/Pages/chapter11.xcplaygroundpage/Contents.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+enum Student {
+    case elementary, middle, high
+    case none
+    
+    init() {
+        self = .none
+    }
+    
+    init(koreanAge: Int) {
+        switch koreanAge {
+        case 8...13:
+            self = .elementary
+        case 14...16:
+            self = .middle
+        case 17...19:
+            self = .high
+        default:
+            self = .none
+        }
+    }
+    
+    init(bornAt: Int, currentYear: Int) {
+        self.init(koreanAge: currentYear - bornAt + 1)
+    }
+}
+
+var younger: Student = Student(koreanAge: 16)
+print(younger)
+
+younger = Student(bornAt: 1998, currentYear: 2016)
+print(younger)
+print()
+
+class Person {
+    let name: String
+    var age: Int?
+    
+    init?(name: String) {
+        if name.isEmpty {
+            return nil
+        }
+        
+        self.name = name
+    }
+    
+    init?(name: String, age: Int) {
+        if name.isEmpty || age < 0 {
+            return nil
+        }
+        self.name = name
+        self.age = age
+    }
+}
+
+let yongki: Person? = Person(name: "yongki", age: 99)
+
+if let person: Person = yongki {
+    print(person.name)
+} else {
+    print("Person wasn't init")
+}
+print()
+
---
+++ SwiftMaster.playground/Pages/chapter11_1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,109 @@
+import SwiftUI
+
+enum Student: String {
+    case elementary = "초딩", middle = "중딩", high = "고딩"
+    
+    init?(koreanAge: Int) {
+        switch koreanAge {
+        case 8...13:
+            self = .elementary
+        case 14...16:
+            self = .middle
+        case 17...19:
+            self = .high
+        default:
+            return nil
+        }
+    }
+    
+    init?(bornAt: Int, currentYear: Int) {
+        self.init(koreanAge: currentYear - bornAt + 1)
+    }
+}
+
+var younger: Student? = Student(koreanAge: 20)
+print(younger)
+
+younger = Student(bornAt: 2020, currentYear: 2016)
+print(younger)
+
+younger = Student(rawValue: "대딩")
+print(younger)
+
+younger = Student(rawValue: "고딩")
+print(younger?.rawValue)
+
+
+struct Student2 {
+    var name: String?
+    var number: Int?
+}
+
+class SchoolClass {
+    var students: [Student2] = {
+        var arr: [Student2] = [Student2]()
+        
+        for num in 1...15 {
+            var student: Student2 = Student2(name: nil, number: num)
+            arr.append(student)
+        }
+        
+        return arr
+    }()
+}
+
+let myClass: SchoolClass = SchoolClass()
+print(myClass.students[0].name)
+print(myClass.students[0].number!)
+print(myClass.students.count)
+print()
+
+class SomeClass {
+    deinit {
+        print("instance will be deallocated immediately")
+    }
+}
+
+var instance: SomeClass? = SomeClass()
+print("시점확인1")
+instance = nil
+print("시점확인2")
+print()
+
+class FileManager {
+    var fileName: String
+    
+    init(fileName: String) {
+        self.fileName = fileName
+    }
+    
+    func openFile() {
+        print("Open File: \(self.fileName)")
+    }
+    
+    func modifyFile() {
+        print("Modify File: \(self.fileName)")
+    }
+    
+    func writeFile() {
+        print("Write File: \(self.fileName)")
+    }
+    
+    func closeFile() {
+        print("Close File: \(self.fileName)")
+    }
+    
+    deinit {
+        print("Deinit instance")
+        self.writeFile()
+        self.closeFile()
+    }
+}
+
+var fileManager: FileManager? = FileManager(fileName: "abc.txt")
+if let manager: FileManager = fileManager {
+    manager.openFile()
+    manager.modifyFile()
+}
+
+fileManager = nil
