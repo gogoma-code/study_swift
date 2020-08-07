5c30ce7f7166c7e49e4e729fe5cfc49b0f39e55a
lyk-brave <officeyongki@gmail.com>
2020-08-07 17:10:10 +0900

    chapter14

---
+++ SwiftMaster.playground/Pages/chapter14.xcplaygroundpage/Contents.swift
@@ -0,0 +1,152 @@
+
+import SwiftUI
+
+class Room {
+    var number: Int
+    
+    init(number: Int) {
+        self.number = number
+    }
+}
+
+class Building {
+    var name: String
+    var room: Room?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct Address {
+    var province: String
+    var city: String
+    var street: String
+    var building: Building?
+    var detailAddress: String?
+}
+
+class Person {
+    var name: String
+    var address: Address?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+let yongki: Person = Person(name: "yongki")
+let yongkiRoomViaOptionalChaining: Int? = yongki.address?.building?.room?.number
+//let yongkiRoomViaOptionalUnwraping: Int = yongki.address?.building?.room?.number // error
+
+var roomNumber: Int? = nil
+if let yongkiAddress: Address = yongki.address {
+    if let yongkiBuilding: Building = yongkiAddress.building {
+        if let yongkiRoom: Room = yongkiBuilding.room {
+            roomNumber = yongkiRoom.number
+        }
+    }
+}
+
+if let number: Int = roomNumber {
+    print(number)
+} else {
+    print("number is nil")
+}
+print()
+
+if let roomNum: Int = yongki.address?.building?.room?.number {
+    print(roomNum)
+} else {
+    print("roomNum is nil")
+}
+print()
+
+yongki.address?.building?.room?.number = 505
+print(yongki.address?.building?.room?.number)
+
+yongki.address = Address(province: "서울특별시", city: "노원구", street: "상계로", building: nil, detailAddress: nil)
+yongki.address?.building = Building(name: "곰굴")
+yongki.address?.building?.room = Room(number: 510)
+print(yongki.address?.building?.room?.number)
+if let roomNum: Int = yongki.address?.building?.room?.number {
+    print(roomNum)
+} else {
+    print("roomNum is nil")
+}
+print()
+print()
+
+
+struct Address2 {
+    var province: String
+    var city: String
+    var street: String
+    var building: Building?
+    var detailAddress: String?
+    
+    init(province: String, city: String, street: String) {
+        self.province = province
+        self.city = city
+        self.street = street
+    }
+    
+    func fullAddress() -> String? {
+        var restAddress: String? = nil
+        
+        if let buildingInfo: Building = self.building {
+            restAddress = buildingInfo.name
+        } else if let detail = self.detailAddress {
+            restAddress = detail
+        }
+        
+        if let rest: String = restAddress {
+            var fullAddress: String = self.province
+            fullAddress += " " + self.city
+            fullAddress += " " + self.street
+            fullAddress += " " + rest
+            
+            return fullAddress
+        } else {
+            return nil
+        }
+    }
+    
+    func printAddress() {
+        if let address: String = self.fullAddress() {
+            print(address)
+        }
+    }
+}
+
+class Person2 {
+    var name: String
+    var address: Address2?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+
+let yongki2: Person2 = Person2(name: "yongki")
+yongki2.address?.fullAddress()?.isEmpty
+yongki2.address?.printAddress()
+
+yongki2.address = Address2(province: "서울특별시", city: "노원구", street: "상계로")
+yongki2.address?.building = Building(name: "곰굴")
+yongki2.address?.building?.room = Room(number: 510)
+
+yongki2.address?.fullAddress()?.isEmpty
+yongki2.address?.printAddress()
+print()
+
+let optionalAry: [Int]? = [1, 2, 3]
+optionalAry?[1]
+
+var optionalDictionary: [String: [Int]]? = [String: [Int]]()
+optionalDictionary?["numberAry"] = optionalAry
+optionalDictionary //(key "numberAry", [1, 2, 3])
+optionalDictionary?["numberAry"] // [1, 2, 3]
+optionalDictionary?["numberAry"]?[2] // 3
+
---
+++ SwiftMaster.playground/Pages/chapter14_1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+import SwiftUI
+
+for i in 0...3 {
+    if i == 2 {
+        print(i)
+    } else {
+        continue
+    }
+}
+print()
+
+for i in 0...3 {
+    guard i == 2 else {
+        continue
+    }
+    print(i)
+}
+print()
+
+func greet(_ person: [String: String]) {
+    guard let name: String = person["name"] else {
+        return
+    }
+    print("Hello \(name)!")
+    
+    guard let location: String = person["location"] else {
+        print("I hope the weather is nice near you")
+        return
+    }
+    print("I hope the weather is nice in \(location)")
+}
+
+var personInfo: [String: String] = [String: String]()
+personInfo["name"] = "Jenny"
+greet(personInfo)
+print()
+
+personInfo["location"] = "Korea"
+greet(personInfo)
+print()
+
+func enterClub(name: String?, age: Int?) {
+    guard let name: String = name, let age: Int = age, age > 19, name.isEmpty == false else {
+        print("You are too young to enter the club")
+        return
+    }
+    
+    print("Welcome \(name)")
+}
+
+enterClub(name: "yongki", age: 29)
+enterClub(name: "yongki", age: 15)
+enterClub(name: "", age: 29)
+enterClub(name: "yongki", age: nil)
+print()
+
+let first: Int = 3
+let second: Int = 5
+
