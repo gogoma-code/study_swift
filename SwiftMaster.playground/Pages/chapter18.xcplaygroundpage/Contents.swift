c6023416b7abf6ea31cf7d7a5818e1d190739583
lyk-brave <officeyongki@gmail.com>
2020-08-09 18:28:11 +0900

    chapter 18 and 19

---
+++ SwiftMaster.playground/Pages/chapter18.xcplaygroundpage/Contents.swift
@@ -0,0 +1,72 @@
+import SwiftUI
+
+class Person {
+    var name: String = ""
+    var age: Int = 0
+    
+    var koreanAge: Int {
+        get {
+            return self.age + 1
+        }
+        
+        set {
+            self.koreanAge = newValue
+        }
+    }
+    
+    var introduction: String {
+        return "이름 : \(name), 나이: \(age)"
+    }
+    
+    func speak() {
+        print("가나다라마바사")
+    }
+}
+
+let yongki: Person = Person()
+yongki.name = "yongki"
+yongki.age = 99
+print(yongki.introduction)
+yongki.speak()
+print()
+
+class Student: Person {
+    var grade: String = "F"
+    
+    override var koreanAge: Int {
+        get {
+            return super.koreanAge
+        }
+        
+        set {
+            self.age = newValue - 1
+        }
+    }
+    
+    func study() {
+        print("study hard...")
+    }
+    
+    override func speak() {
+        print("에베베베")
+    }
+}
+
+let hyemi: Student = Student()
+hyemi.name = "hyemi"
+hyemi.age = 10
+hyemi.grade = "A"
+print(hyemi.introduction)
+hyemi.speak()
+hyemi.study()
+print()
+
+class UniversityStudent: Student {
+    var major: String = ""
+}
+
+let jenny: UniversityStudent = UniversityStudent()
+jenny.major = "Art"
+jenny.speak()
+jenny.study()
+print()
